d7c00e8332e08f40d1e31020faadc43eb662935a
Nathan Chapelle <nathanchapelle76@gmail.com>
2023-02-27 20:16:53 +0100

    fixed a bug that when a dofus or trophy was treated, the code cut and got flush

--- src/Services/ItemService.php
+++ src/Services/ItemService.php
@@ -36,59 +36,58 @@ class ItemService
     {
         foreach ($this->itemTypes as $items) {
             foreach ($items as $item) {
-                if ($item['type'] == 'Dofus' || $item['type'] === 'Trophée') {
-                    break;
-                }
-                $newItem = new Item();          
-                $newItem->setAnkamaId($item['ankamaId']);
-                $newItem->setName($item['name']);
-                $newItem->setLevel($item['level']);
-                $newItem->setType($item['type']);
-                $newItem->setImgUrl($item['imgUrl']);
-                $newItem->setUrl($item['url']);
-                if(array_key_exists('statistics',$item)){
-                    foreach ($item['statistics'] as $itemStat) {
-                        foreach ($this->statistics as $statistic) {
-                            if ($statistic->getName() === key($itemStat) && $itemStat[$statistic->getName()]['min'] > 0) {
-                                $newItemStat = new ItemStatistic();
-                                $newItemStat->setStatistic($statistic);
-                                $newItemStat->setItem($newItem);
-                                $itemStat[$statistic->getName()]['max']
-                                    ? $quantity = $itemStat[$statistic->getName()]['max']
-                                    : $quantity = $itemStat[$statistic->getName()]['min'];
-                                $newItemStat->setQuantity($quantity);
-                                $this->manager->persist($newItemStat);
-                                $newItem->addItemStatistic($newItemStat);
+                if (!($item['type'] === 'Dofus' || $item['type'] === 'Trophée')) {
+                    $newItem = new Item();          
+                    $newItem->setAnkamaId($item['ankamaId']);
+                    $newItem->setName($item['name']);
+                    $newItem->setLevel($item['level']);
+                    $newItem->setType($item['type']);
+                    $newItem->setImgUrl($item['imgUrl']);
+                    $newItem->setUrl($item['url']);
+                    if(array_key_exists('statistics',$item)){
+                        foreach ($item['statistics'] as $itemStat) {
+                            foreach ($this->statistics as $statistic) {
+                                if ($statistic->getName() === key($itemStat) && $itemStat[$statistic->getName()]['min'] > 0) {
+                                    $newItemStat = new ItemStatistic();
+                                    $newItemStat->setStatistic($statistic);
+                                    $newItemStat->setItem($newItem);
+                                    $itemStat[$statistic->getName()]['max']
+                                        ? $quantity = $itemStat[$statistic->getName()]['max']
+                                        : $quantity = $itemStat[$statistic->getName()]['min'];
+                                    $newItemStat->setQuantity($quantity);
+                                    $this->manager->persist($newItemStat);
+                                    $newItem->addItemStatistic($newItemStat);
+                                }
                             }
-                        }
-                    };
-                }
-                foreach ($item['recipe'] as $ingredients) {
-                    foreach ($ingredients as $ingredient) {
-                        $itemIngredient = new ItemIngredient();
-                        $newItem->addItemIngredient($itemIngredient);
-                        $itemIngredient->setItem($newItem);
-                        $itemIngredient->setQuantity($ingredient['quantity']);
-                        $ankama_ids = [];
-                        foreach ($this->ingredientsArray as $ingredientsArray) {
-                            $ankama_ids[] = $ingredientsArray->getAnkamaId();
-                        }
-                        if (!$this->ingredientsArray || !in_array($ingredient['ankamaId'], $ankama_ids)) {
-                            $newIngredient = new Ingredient();
-                            $newIngredient->setName(key($ingredients));
-                            $newIngredient->setAnkamaId($ingredient['ankamaId']);
-                            $newIngredient->setImgUrl($ingredient['imgUrl']);
-                            $this->manager->persist($newIngredient);
-            
-                            $itemIngredient->setIngredient($newIngredient);
-                        } else {
-                            $fetchedIngredient = $this->ingredientRepository->findByAnkamaId($ingredient['ankamaId']);
-                            $itemIngredient->setIngredient($fetchedIngredient[0]);
-                        }
-                        $this->manager->persist($itemIngredient);
+                        };
                     }
+                    foreach ($item['recipe'] as $ingredients) {
+                        foreach ($ingredients as $ingredient) {
+                            $itemIngredient = new ItemIngredient();
+                            $newItem->addItemIngredient($itemIngredient);
+                            $itemIngredient->setItem($newItem);
+                            $itemIngredient->setQuantity($ingredient['quantity']);
+                            $ankama_ids = [];
+                            foreach ($this->ingredientsArray as $ingredientsArray) {
+                                $ankama_ids[] = $ingredientsArray->getAnkamaId();
+                            }
+                            if (!$this->ingredientsArray || !in_array($ingredient['ankamaId'], $ankama_ids)) {
+                                $newIngredient = new Ingredient();
+                                $newIngredient->setName(key($ingredients));
+                                $newIngredient->setAnkamaId($ingredient['ankamaId']);
+                                $newIngredient->setImgUrl($ingredient['imgUrl']);
+                                $this->manager->persist($newIngredient);
+                
+                                $itemIngredient->setIngredient($newIngredient);
+                            } else {
+                                $fetchedIngredient = $this->ingredientRepository->findByAnkamaId($ingredient['ankamaId']);
+                                $itemIngredient->setIngredient($fetchedIngredient[0]);
+                            }
+                            $this->manager->persist($itemIngredient);
+                        }
+                    }
+                    $this->manager->persist($newItem);
                 }
-                $this->manager->persist($newItem);
             }
             $this->manager->flush();
             }
